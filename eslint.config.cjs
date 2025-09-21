982e4f1f904cbcd9b673a14affee3b9f799439a8
= <=>
2025-09-20 22:16:56 -0300

    Refinando eslint

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -3,9 +3,30 @@ const js = require("@eslint/js");
 module.exports = [
   js.configs.recommended,
   {
+    languageOptions: {
+      globals: {
+        // Node.js globals
+        require: "readonly",
+        module: "readonly",
+        process: "readonly",
+        __dirname: "readonly",
+        console: "readonly",
+        // Jest globals
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        test: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        // Cypress globals
+        cy: "readonly",
+        Cypress: "readonly",
+      },
+    },
     rules: {
       semi: ["error", "always"],
       quotes: ["error", "double"],
     },
   },
 ];
+
