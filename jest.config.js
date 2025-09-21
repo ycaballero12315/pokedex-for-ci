0618e7a3c01ffe9f95351a9ca51331782f28cd9c
= <=>
2025-09-20 23:19:02 -0300

    Delete testrun

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   testEnvironment: "jest-environment-jsdom", // ✅ ahora lo encuentra
-  testRunner: "jest-circus/runner",
   transform: {
     "^.+\\.(js|jsx)$": "babel-jest"
   },
